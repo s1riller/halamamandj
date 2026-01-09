f03ceb5f7010e6238028de4b00d416eb5430817c
Saveliy <miheev.saveliy.2002@gmail.com>
2026-01-10 03:48:34 +0800

    c

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 interface User {
   id: string;
