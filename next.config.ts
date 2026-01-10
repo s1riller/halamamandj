ce50069ce4edf1d2089bc2ee49d3d08fedd69d87
Saveliy <miheev.saveliy.2002@gmail.com>
2026-01-10 16:36:25 +0800

    Fix 4 change next config delete docs

--- next.config.ts
+++ next.config.ts
@@ -6,18 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["fakestoreapi.com"],
   },
-  exportPathMap: async function(defaultPathMap) {
-    const { ...paths } = defaultPathMap;
-    
-    // Удаляем API routes из экспорта
-    Object.keys(paths).forEach(path => {
-      if (path.startsWith('/api/')) {
-        delete paths[path];
-      }
-    });
-    
-    return paths;
-  },
 };
 
 export default nextConfig;
